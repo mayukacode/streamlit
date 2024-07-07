b3ae5150f8c709196a135e8c7ba36e8d283035f2
mayukacode <2rukochan@gmail.com>
2024-07-07 17:19:38 +0900

    別パターン

--- app3/app.py
+++ app3/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import time
 
 st.title("ガチャガチャをまわせ♪")
 
@@ -17,24 +16,35 @@ st.image("static/gachagacha.png")
 
 if st.button("まわす"):
     with st.spinner('ちょっと待ってね...'):
+        import time
         time.sleep(2)
-        
+
         st.image("static/gach.png")
         time.sleep(2)
-        
-        options = ["はずれ","ケーキ","ドーナツ","チョコ","キャンディ","つみき","ぬいぐるみ","しんかんせん","ラッパ"]
+
+        options = ["はずれ", "ケーキ", "ドーナツ", "チョコ", "キャンディ", "つみき", "ぬいぐるみ", "しんかんせん", "ラッパ"]
         luck = np.random.choice(options, 1, p=[0.4, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])[0]
-        
+
         if luck == "はずれ":
             image = "static/hazure.png"
         elif luck == "ケーキ":
             image = "static/shortcake.png"
         elif luck == "ドーナツ":
             image = "static/donut.png"
-        # 他のケースも同様に処理
+        elif luck == "チョコ":
+            image = "static/itachoco.png"
+        elif luck == "キャンディ":
+            image = "static/candy.png"
+        elif luck == "つみき":
+            image = "static/tsumiki.png"
+        elif luck == "ぬいぐるみ":
+            image = "static/bear.png"
+        elif luck == "しんかんせん":
+            image = "static/shinkansen.png"
+        elif luck == "ラッパ":
+            image = "static/rappa.png"
 
-        st.image(f"static/{image}")
+        st.image(image)
 
 if st.button("もういちど"):
-    placeholder = st.empty()
-    placeholder.empty()
+    st.caching.clear_cache()
