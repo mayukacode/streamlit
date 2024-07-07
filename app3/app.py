b745266a04064987850123e2ea0ca7a2ecdf83f4
mayukacode <2rukochan@gmail.com>
2024-07-07 17:05:46 +0900

    修正

--- app3/app.py
+++ app3/app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import numpy as np
+import time
 
 st.title("ガチャガチャをまわせ♪")
 
-
 selected_animal = st.sidebar.selectbox("だれにする？", ["🐶いぬ🐶", "🐱ねこ🐱", "🐰うさぎ🐰"])
 
-if selected_animal == "🐶いぬ🐶": 
+if selected_animal == "🐶いぬ🐶":
     st.sidebar.image("dog.png")
-elif  selected_animal == "🐱ねこ🐱": 
+elif selected_animal == "🐱ねこ🐱":
     st.sidebar.image("cat.png")
-elif selected_animal == "🐰うさぎ🐰": 
+elif selected_animal == "🐰うさぎ🐰":
     st.sidebar.image("usagi.png")
 
-
 st.image("gachagacha.png")
+
 if st.button("まわす"):
     with st.spinner('ちょっと待ってね...'):
         time.sleep(2)
@@ -23,7 +23,7 @@ if st.button("まわす"):
         time.sleep(2)
         
         options = ["はずれ","ケーキ","ドーナツ","チョコ","キャンディ","つみき","ぬいぐるみ","しんかんせん","ラッパ"]
-        luck = np.random.choice(options, 1, p=[0.4,0.075,0.075,0.075,0.075,0.075,0.075,0.075,0.075])[0]
+        luck = np.random.choice(options, 1, p=[0.4, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])[0]
         
         if luck == "はずれ":
             image = "hazure.png"
@@ -34,6 +34,7 @@ if st.button("まわす"):
         # 他のケースも同様に処理
 
         st.image(image)
-        if st.button("もういちど"):
-            placeholder = st.empty()
-            placeholder.empty()
+
+if st.button("もういちど"):
+    placeholder = st.empty()
+    placeholder.empty()
