e0b4e82b8dcbc705a36ecf1c7abcb0914dc8698d
mayukacode <2rukochan@gmail.com>
2024-07-07 18:20:08 +0900

    修正

--- app3/app.py
+++ app3/app.py
@@ -15,26 +15,38 @@ elif selected_animal == "🐰うさぎ🐰":
 
 st.image("gachagacha.png")
 
+def spin_gacha():
+    st.image("gach.png")
+    time.sleep(2)
+
+    options = ["はずれ", "ケーキ", "ドーナツ", "チョコ", "キャンディ", "つみき", "ぬいぐるみ", "しんかんせん", "ラッパ"]
+    luck = np.random.choice(options, 1, p=[0.4, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])[0]
+
+    if luck == "はずれ":
+        image = "hazure.png"
+    elif luck == "ケーキ":
+        image = "shortcake.png"
+    elif luck == "ドーナツ":
+        image = "donut.png"
+    elif luck == "チョコ":
+        image = "choco.png"
+    elif luck == "キャンディ":
+        image = "candy.png"
+    elif luck == "つみき":
+        image = "blocks.png"
+    elif luck == "ぬいぐるみ":
+        image = "plush.png"
+    elif luck == "しんかんせん":
+        image = "shinkansen.png"
+    elif luck == "ラッパ":
+        image = "trumpet.png"
+
+    st.image(image)
+
 if st.button("まわす"):
     with st.spinner('ちょっと待ってね...'):
         time.sleep(2)
-        
-        st.image("gach.png")
-        time.sleep(2)
-        
-        options = ["はずれ","ケーキ","ドーナツ","チョコ","キャンディ","つみき","ぬいぐるみ","しんかんせん","ラッパ"]
-        luck = np.random.choice(options, 1, p=[0.4, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])[0]
-        
-        if luck == "はずれ":
-            image = "hazure.png"
-        elif luck == "ケーキ":
-            image = "shortcake.png"
-        elif luck == "ドーナツ":
-            image = "donut.png"
-        # 他のケースも同様に処理
-
-        st.image(image)
+        spin_gacha()
 
 if st.button("もういちど"):
-    placeholder = st.empty()
-    placeholder.empty()
+    st.experimental_rerun()
