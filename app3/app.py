00df657af2458216b9c6de3ce04c39b79fbb2a33
mayukacode <2rukochan@gmail.com>
2024-07-07 17:30:31 +0900

    修正

--- app3/app.py
+++ app3/app.py
@@ -6,33 +6,33 @@ st.title("ガチャガチャをまわせ♪")
 selected_animal = st.sidebar.selectbox("だれにする？", ["🐶いぬ🐶", "🐱ねこ🐱", "🐰うさぎ🐰"])
 
 if selected_animal == "🐶いぬ🐶": 
-    st.sidebar.image("static/dog.png")
+    st.sidebar.image("dog.png")
 elif selected_animal == "🐱ねこ🐱": 
-    st.sidebar.image("static/cat.png")
+    st.sidebar.image("cat.png")
 elif selected_animal == "🐰うさぎ🐰": 
-    st.sidebar.image("static/usagi.png")
+    st.sidebar.image("usagi.png")
 
-st.image("static/gachagacha.png")
+st.image("gachagacha.png")
 
 if st.button("まわす"):
     with st.spinner('ちょっと待ってね...'):
         time.sleep(2)
         
-        st.image("static/gach.png")
+        st.image("gach.png")
         time.sleep(2)
         
         options = ["はずれ","ケーキ","ドーナツ","チョコ","キャンディ","つみき","ぬいぐるみ","しんかんせん","ラッパ"]
         luck = np.random.choice(options, 1, p=[0.4, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])[0]
         
         if luck == "はずれ":
-            image = "static/hazure.png"
+            image = "hazure.png"
         elif luck == "ケーキ":
-            image = "static/shortcake.png"
+            image = "shortcake.png"
         elif luck == "ドーナツ":
-            image = "static/donut.png"
+            image = "donut.png"
         # 他のケースも同様に処理
 
-        st.image(f"static/{image}")
+        st.image(image)
 
 if st.button("もういちど"):
     st.caching.clear_cache()
