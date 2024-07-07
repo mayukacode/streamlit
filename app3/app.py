9b0e09a2059aca41e175a2ba8191fd2243b0a7e7
mayukacode <2rukochan@gmail.com>
2024-07-07 18:26:27 +0900

    デプロイできた？

--- app3/app.py
+++ app3/app.py
@@ -1,38 +1,51 @@
 import streamlit as st
 import numpy as np
+import time
 
 st.title("ガチャガチャをまわせ♪")
+ 
 
-selected_animal = st.sidebar.selectbox("だれにする？", ["🐶いぬ🐶", "🐱ねこ🐱", "🐰うさぎ🐰"])
+selected_animal = st.sidebar.selectbox("だれにする？", ["🐶　いぬ　🐶", "🐱　ねこ　🐱", "🐰　うさぎ　🐰"])
 
-if selected_animal == "🐶いぬ🐶": 
-    st.sidebar.image("dog.png")
-elif selected_animal == "🐱ねこ🐱": 
-    st.sidebar.image("cat.png")
-elif selected_animal == "🐰うさぎ🐰": 
-    st.sidebar.image("usagi.png")
+if selected_animal == "🐶　いぬ　🐶": 
+   st.sidebar.image("dog.png")
+elif  selected_animal == "🐱　ねこ　🐱": 
+      st.sidebar.image("cat.png")
+elif selected_animal == "🐰　うさぎ　🐰": 
+      st.sidebar.image("usagi.png")
+
 
 st.image("gachagacha.png")
 
 if st.button("まわす"):
-    with st.spinner('ちょっと待ってね...'):
-        time.sleep(2)
-        
-        st.image("gach.png")
-        time.sleep(2)
-        
-        options = ["はずれ","ケーキ","ドーナツ","チョコ","キャンディ","つみき","ぬいぐるみ","しんかんせん","ラッパ"]
-        luck = np.random.choice(options, 1, p=[0.4, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])[0]
-        
-        if luck == "はずれ":
-            image = "hazure.png"
-        elif luck == "ケーキ":
-            image = "shortcake.png"
-        elif luck == "ドーナツ":
-            image = "donut.png"
-        # 他のケースも同様に処理
+  time.sleep(2)
 
-        st.image(image)
+  st.image("gach.png")
+  time.sleep(2)
 
+  options = ["はずれ","ケーキ","ドーナツ","チョコ","キャンディ","つみき","ぬいぐるみ","しんかんせん","ラッパ"]
+  luck = np.random.choice(options, 1, p=[0.4,0.075,0.075,0.075,0.075,0.075,0.075,0.075,0.075])[0]
+ 
+  if luck=="はずれ":
+     image = "hazure.png"
+  elif luck=="ケーキ":
+       image = "shortcake.png"
+  elif luck=="ドーナツ":
+       image = "donut.png"
+  elif luck=="チョコ":
+       image = "itachoco.png"
+  elif luck=="キャンディ":
+     image = "candy.png"
+  elif luck=="つみき":
+     image = "tsumiki.png" 
+  elif luck=="ぬいぐるみ":
+     image = "bear.png"
+  elif luck=="しんかんせん":
+     image = "shinkansen.png"
+  elif luck=="ラッパ":
+     image = "rappa.png"
+
+  st.image(image)
 if st.button("もういちど"):
-    st.caching.clear_cache()
+  placeholder = st.empty()
+  placeholder.empty()
